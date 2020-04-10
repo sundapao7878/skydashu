0c44518c3e779c544c8ee73c9702085c48a570e4
sundapao7878 <63441849+sundapao7878@users.noreply.github.com>
2020-04-10 14:29:20 +0800

    Add files via upload

---
+++ jsGetCode.js
@@ -0,0 +1,15 @@
+OnGetCode = function ( nType )
+{
+    let strCode = "";
+    //public
+    if( nType == 0 )
+    {
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUzXsxZIEKFFo9IQKaRydEP6fpSKj53Zwblngr1zuAokh4HK5yjSTPH7B/7orYMgo7eWI7p26dzgUlFMCp4+FWkYUUQMMuTSoIqUNYc58hhXgw==";
+    }
+    //Dev
+    else
+    {
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUxC91Q7bmKIfhodm61VMcduPMgelizWyWnt43Hdz4togpKxk2EqCnC9Sdm6wSwrPPmrl69Q+eDgud5GNGH9jXWO1lrzMhbMYxwLuuG0+6BOuw==";
+    }
+    return  strCode;
+}
